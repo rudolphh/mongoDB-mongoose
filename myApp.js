de81f6c3e28045d409561efc391de8c591cd1d88
Rudolph Hernandez <rudolpharthur@gmail.com>
2017-04-20 02:14:40 -0700

    add dotenv to package.json and require in myApp.js for use with private .env

--- myApp.js
+++ myApp.js
@@ -11,8 +11,12 @@
 // Add `mongodb` and `mongoose` to the project's `package.json`. Then require 
 // `mongoose`. Store your **mLab** database URI in the private `.env` file 
 // as `MONGO_URI`. Connect to the database using `mongoose.connect(<Your URI>)`
+var dotenv = require('dotenv');
+dotenv.load();
+
 var mongoose = require('mongoose');
 mongoose.connect(process.env.MONGO_URI);
+
 
 /** # SCHEMAS and MODELS #
 /*  ====================== */
